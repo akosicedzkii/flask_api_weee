9563e4ac6ba337d7ea64cf95906dd17588b64c38
Cederic Martinez <cedzkii81@gmail.com>
2019-09-09 21:22:13 +0800

    added more swagger ui

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_restplus import Api, Resource
+from flask_swagger_ui import get_swaggerui_blueprint
 
 authorizations = {
     'apikey': {
@@ -15,8 +16,19 @@ authorizations = {
 app = Flask(__name__)
 api = Api(app = app,authorizations=authorizations,description='DevOps APIs',title = "DevOps APIs" )
 
+SWAGGER_URL='/swagger'
+API_URL ='/swagger.json'
 name_space = api.namespace('auth', description='Auth APIs')
 
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        "app-name" : "Test"
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint,url_prefix=SWAGGER_URL)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:rootpassword@172.22.0.2/flask_api'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'some-secret-string'
@@ -39,6 +51,7 @@ def check_if_token_in_blacklist(decrypted_token):
     return models.RevokedTokenModel.is_jti_blacklisted(jti)
 
 import views, models, resources
+
 
 api.add_resource(resources.UserRegistration, '/registration')
 api.add_resource(resources.UserLogin)

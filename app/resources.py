35730650f772bf8c0e5c98ac2b51265c5b78ef73
Cederic Martinez <cedzkii81@gmail.com>
2019-10-12 21:56:24 +0800

    added docker

---
+++ app/resources.py
@@ -0,0 +1,124 @@
+from flask_restful import Resource, reqparse
+from models import UserModel, RevokedTokenModel
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
+import producer
+parser = reqparse.RequestParser()
+parser.add_argument('username', help = 'This field cannot be blank', required = True)
+parser.add_argument('password', help = 'This field cannot be blank', required = True)
+import asyncio
+from run import api,name_space
+from flask_restplus import Api, Resource
+
+class UserRegistration(Resource):
+    @api.doc(responses={ 200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error' }, 
+			 params={ 'username': 'Username of the user' , 'password' : 'Password of the User' })
+    def post(self):
+        data = parser.parse_args()
+        
+        if UserModel.find_by_username(data['username']):
+            return {'message': 'User {} already exists'.format(data['username'])}
+        
+        new_user = UserModel(
+            username = data['username'],
+            password = UserModel.generate_hash(data['password'])
+        )
+        
+        try:
+            new_user.save_to_db()
+            access_token = create_access_token(identity = data['username'])
+            refresh_token = create_refresh_token(identity = data['username'])
+            return {
+                'message': 'User {} was created'.format(data['username']),
+                'date_created' : str(new_user.date_created),
+                'access_token': access_token,
+                'refresh_token': refresh_token
+                }
+        except:
+            return {'message': 'Something went wrong'}, 500
+
+@name_space.route("/login")
+class UserLogin(Resource):
+    @api.doc(responses={ 200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error' }, 
+			 params={ 'username': 'Username of the user' , 'password' : 'Password of the User' })
+    def post(self):
+        data = parser.parse_args()
+        current_user = UserModel.find_by_username(data['username'])
+
+        if not current_user:
+            return {'message': 'User {} doesn\'t exist'.format(data['username'])}
+        
+        if UserModel.verify_hash(data['password'], current_user.password):
+            access_token = create_access_token(identity = data['username'])
+            refresh_token = create_refresh_token(identity = data['username'])
+            return {
+                'message': 'Logged in as {}'.format(current_user.username),
+                'access_token': access_token,
+                'refresh_token': refresh_token
+                }
+        else:
+            return {'message': 'Wrong credentials'}
+
+@api.doc(security='apikey')
+@name_space.route("/logout/access")
+class UserLogoutAccess(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        try:
+            revoked_token = RevokedTokenModel(jti = jti)
+            revoked_token.add()
+            return {'message': 'Access token has been revoked'}
+        except:
+            return {'message': 'Something went wrong'}, 500
+
+@api.doc(security='apikey')
+@name_space.route("/logout/refresh")
+class UserLogoutRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        try:
+            revoked_token = RevokedTokenModel(jti = jti)
+            revoked_token.add()
+            return {'message': 'Refresh token has been revoked'}
+        except:
+            return {'message': 'Something went wrong'}, 500
+
+@api.doc(security='apikey')
+@name_space.route("/token/refresh")
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity = current_user)
+        return {'access_token': access_token}
+
+
+class AllUsers(Resource):
+    def get(self):
+        return UserModel.return_all()
+    
+    def delete(self):
+        return UserModel.delete_all()
+@api.doc(security='apikey')
+class SecretResource(Resource):
+    @jwt_required
+    def get(self):
+        return {
+            'answer': 42
+        }
+        
+@api.doc(security='apikey')        
+class SendEmail(Resource):
+    @jwt_required
+    @api.doc(responses={ 200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error' }, 
+			 params={ 'subject': 'Subject of the email' , 'message' : 'Email message' })
+    def post(self):
+        custom_parser = reqparse.RequestParser()
+        custom_parser.add_argument('message', help = 'This field cannot be blank', required = True)
+        custom_parser.add_argument('subject', help = 'This field cannot be blank', required = True)
+        data = custom_parser.parse_args()
+        producer.queue_message(data["message"],data["subject"])
+        return {
+            'message': 'Message Sent'
+        }
